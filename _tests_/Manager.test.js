06c15b86d64b248a1ecaf61f66523480ba61cbe4
Monineath Sar <monineathsar@gmail.com>
2022-07-07 17:35:52 -0700

    Manager test passed

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -7,8 +7,8 @@ describe("Manager", () => {
         expect(employee.name).toEqual(expect.any(String));
         expect(employee.id).toEqual(expect.any(String));
         expect(employee.email).toEqual(expect.any(String));
-        expect(employee.officeNumber.split("-").join("")).toHaveLength(10);
-        expect(Number.isInteger(Number(employee.officeNumber.split("-").join("")))).toEqual(true);
+        expect(employee.officeNumber).toEqual(expect.any(String));
+        
     })
     test("checks manager class methods", () => {
         const employee = new Manager("Katie Anderson", "0064", "katieanderson@gmail.com", "555-555-5555");
@@ -19,4 +19,14 @@ describe("Manager", () => {
         expect(employee.getOfficeNumber()).toBe(employee.officeNumber);
         expect(employee.getRole()).toBe("Manager");
     })
+    test("checks to ensure offce number is a 10-digit phone number", () => {
+        const employee = new Manager("Katie Anderson", "0064", "katieanderson@gmail.com", "555-555-5555");
+        expect(employee.officeNumber.split("-").join("")).toHaveLength(10);
+    })
+    test("checks if office number are numbers and not a string", () => {
+        const employee = new Manager("Katie Anderson", "0064", "katieanderson@gmail.com", "555-555-5555");
+
+        expect(Number.isInteger(Number(employee.officeNumber.split("-").join("")))).toEqual(true);
+    })
+       
 })
