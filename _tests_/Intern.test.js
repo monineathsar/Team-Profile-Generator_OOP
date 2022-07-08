0fffbffd9f834349b7b4e4a1b8523f2276daf0a6
Monineath Sar <monineathsar@gmail.com>
2022-07-07 18:00:00 -0700

    Intern test passed

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -1,4 +1,4 @@
-const Intern = require('../lib/InternClass');
+const { Intern } = require('../lib/InternClass');
 
 describe("Intern", () => {
     test("checks intern array object", () => {
@@ -8,6 +8,12 @@ describe("Intern", () => {
         expect(employee.id).toEqual(expect.any(String));
         expect(employee.email).toEqual(expect.any(String));
         expect(employee.school).toEqual(expect.any(String));
+    })
+    test("checks employee id is a number with correct length", () => {
+        const employee = new Intern("Cloe Swift", "0506", "cloeswift@gmail.com", "UC Berkeley");
+
+        expect(employee.id).toHaveLength(4);
+        expect(Number.isInteger(Number(employee.id))).toEqual(true);
     })
     test("checks intern class methods", () => {
         const employee = new Intern("Cloe Swift", "0506", "cloeswift@gmail.com", "UC Berkeley");
